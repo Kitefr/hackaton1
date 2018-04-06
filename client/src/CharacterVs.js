8d0e0ff4f45f89e09d1d9e4ac7eab194ee3c76fd
Kitefr <kitefr@gmail.com>
2018-04-06 08:12:13 +0200

    correct some typo

--- client/src/CharacterVs.js
+++ client/src/CharacterVs.js
@@ -1,37 +1,33 @@
 import React, { Component } from "react";
-import characters from './characters.json';
-import styled from 'styled-components';
-
+import characters from "./assets/characters.json";
+import styled from "styled-components";
 
 const CharacterBox = styled.div`
-    width:110px;
-    height:110px;
-`
+  width: 110px;
+  height: 110px;
+`;
 
 const CharacterImg = styled.img`
-    width:100%;
-    max-height:100%;
-`
+  width: 100%;
+  max-height: 100%;
+`;
 
 class CharacterVs extends Component {
+  state = {
+    character: characters[0]
+  };
 
-    state={
-        character:characters[0]
-    };
+  render() {
+    console.log(characters);
 
-    
-
-    render() {
-        console.log(characters);
-        
-        return (
-            <div>
-                <CharacterBox>
-                    <CharacterImg src={this.state.character.images.sm}></CharacterImg>
-                </CharacterBox>
-            </div>
-        );
-    }
+    return (
+      <div>
+        <CharacterBox>
+          <CharacterImg src={this.state.character.images.sm} />
+        </CharacterBox>
+      </div>
+    );
+  }
 }
 
 export default CharacterVs;
